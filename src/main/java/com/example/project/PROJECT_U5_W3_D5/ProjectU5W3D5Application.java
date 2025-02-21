13fd8f27bd9a6ebce412c07b3c5d28d8b5f92c6e
tommaso.panciroli <tommypanci@gmail.com>
2025-02-21 11:34:01 +0100

    Initial commit

---
+++ src/main/java/com/example/project/PROJECT_U5_W3_D5/ProjectU5W3D5Application.java
@@ -0,0 +1,13 @@
+package com.example.project.PROJECT_U5_W3_D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectU5W3D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectU5W3D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project/PROJECT_U5_W3_D5/ProjectU5W3D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project.PROJECT_U5_W3_D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectU5W3D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
